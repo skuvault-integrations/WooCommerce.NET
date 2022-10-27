f6bf11c8d3a296711efee56e3ec17a71161156aa
maxim-skuvault <maxim.strelsov@skuvault.com>
2022-10-27 11:56:28 -0400

    GUARD-2572 Orders Sync Avatax Issue (#18)
    * GUARD-2572 Change the legacy Order tax_lines.rate_id to string, since WooCommerce has non-numeric text there in some cases (AVATAX plugin)
    
    * Add pull request template
    
    * GUARD-2572 Log raw JSON response from WooCommerce when a deserialization error happens
    
    * GUARD-2572 Update PR template based on skuvault-v1 / Staging
    
    * GUARD-2572 Added using for new MemoryStream()

--- RestAPI.cs
+++ RestAPI.cs
@@ -345,9 +345,19 @@ namespace WooCommerceNET
             };
 
             DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T), settings);
-            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
-            T obj = (T)ser.ReadObject(stream);
-            stream.Dispose();
+            T obj;
+            try
+            {
+                using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+                {
+                    obj = (T)ser.ReadObject(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(
+                    "Unable to deserialize JSON response from WooCommerce (see inner exception for more details). Raw JSON response: " + jsonString, ex);
+            }
 
             return obj;
         }

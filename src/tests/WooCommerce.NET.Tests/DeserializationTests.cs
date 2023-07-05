08c7e2b0b715d021d9e31d8e08ea3ae23c653b5e
Mikhail Rassudishkin <mikhail.rassudishkin@skuvault.com>
2023-07-05 14:02:55 +0300

    VT-6099 : updated tests to TestCase

--- src/tests/WooCommerce.NET.Tests/DeserializationTests.cs
+++ src/tests/WooCommerce.NET.Tests/DeserializationTests.cs
@@ -10,121 +10,47 @@ namespace WooCommerce.NET.Tests
 	[TestFixture]
 	public class DeserializationTests
 	{
-		[Test]
-		public void DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreIncorrect()
+		[TestCase(
+			@"..\..\Files\ProductsJsonResponse_WhenDatesAreIncorrect.json",
+			"0001-01-01T00:00:00",
+			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreIncorrect")]
+		[TestCase(
+			@"..\..\Files\ProductsJsonResponse_WhenDatesAreCorrect.json",
+			"2023-11-29T19:03:58",
+			TestName = "DeserializeJSon_ProductsHaveCorrectDateTimeValues_WhenDatesAreCorrect")]
+		[TestCase(
+			@"..\..\Files\ProductsJsonResponse_WhenDatesAreNull.json",
+			"0001-01-01T00:00:00",
+			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreNull")]
+		[TestCase(
+			@"..\..\Files\ProductsJsonResponse_WhenDatesAreEmpty.json",
+			"0001-01-01T00:00:00",
+			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreEmpty")]
+		public void DeserializeJson(string fileName, string expectedDateValue)
 		{
 			// arrange
 			var restApiV3 = new RestAPI("wp-json/wc/v3", "", "");
 
-			var jsonResponse =
-				File.ReadAllText(
-					@"..\..\Files\ProductsJsonResponse_WhenDatesAreIncorrect.json");
+			var jsonResponse = File.ReadAllText(fileName);
 
 			// act
 			var products = restApiV3.DeserializeJSon<List<Product>>(jsonResponse);
 
 			// assert
+			var expectedDate = DateTime.Parse(expectedDateValue);
 			Assert.That(products.Count, Is.EqualTo(1));
-			Assert.That(products[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
-		}
-
-		[Test]
-		public void DeserializeJSon_ProductsHaveCorrectDateTimeValues_WhenDatesAreCorrect()
-		{
-			// arrange
-			var restApiV3 = new RestAPI("wp-json/wc/v3", "", "");
-
-			var jsonResponse =
-				File.ReadAllText(
-					@"..\..\Files\ProductsJsonResponse_WhenDatesAreCorrect.json");
-
-			// act
-			var products = restApiV3.DeserializeJSon<List<Product>>(jsonResponse);
-
-			// assert
-			var correctDateTimeValue = new DateTime(2023, 11, 29, 19, 03, 58);
-			Assert.That(products.Count, Is.EqualTo(1));
-			Assert.That(products[0].date_created, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_created_gmt, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_modified, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_modified_gmt, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_on_sale_from, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_on_sale_from_gmt, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_on_sale_to, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].date_on_sale_to_gmt, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].images[0].date_created, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].images[0].date_created_gmt, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].images[0].date_modified, Is.EqualTo(correctDateTimeValue));
-			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(correctDateTimeValue));
-		}
-
-		[Test]
-		public void DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreNull()
-		{
-			// arrange
-			var restApiV3 = new RestAPI("wp-json/wc/v3", "", "");
-
-			var jsonResponse =
-				File.ReadAllText(
-					@"..\..\Files\ProductsJsonResponse_WhenDatesAreNull.json");
-
-			// act
-			var products = restApiV3.DeserializeJSon<List<Product>>(jsonResponse);
-
-			// assert
-			Assert.That(products.Count, Is.EqualTo(1));
-			Assert.That(products[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
-		}
-		
-		[Test]
-		public void DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreEmpty()
-		{
-			// arrange
-			var restApiV3 = new RestAPI("wp-json/wc/v3", "", "");
-
-			var jsonResponse =
-				File.ReadAllText(
-					@"..\..\Files\ProductsJsonResponse_WhenDatesAreEmpty.json");
-
-			// act
-			var products = restApiV3.DeserializeJSon<List<Product>>(jsonResponse);
-
-			// assert
-			Assert.That(products.Count, Is.EqualTo(1));
-			Assert.That(products[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_from_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].date_on_sale_to_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_created_gmt, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified, Is.EqualTo(DateTime.MinValue));
-			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(DateTime.MinValue));
+			Assert.That(products[0].date_created, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_created_gmt, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_modified, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_modified_gmt, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_on_sale_from, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_on_sale_from_gmt, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_on_sale_to, Is.EqualTo(expectedDate));
+			Assert.That(products[0].date_on_sale_to_gmt, Is.EqualTo(expectedDate));
+			Assert.That(products[0].images[0].date_created, Is.EqualTo(expectedDate));
+			Assert.That(products[0].images[0].date_created_gmt, Is.EqualTo(expectedDate));
+			Assert.That(products[0].images[0].date_modified, Is.EqualTo(expectedDate));
+			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(expectedDate));
 		}
 	}
 }

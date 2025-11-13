8d264b0c794194ffabc9a7e7f064ba22bf0d445d
Reydel Leon Machado <reydel.leon@linnworks.com>
2025-11-13 14:18:33 -0500

    SVCORE-2338 - Fix tests

--- src/tests/WooCommerce.NET.Tests/DeserializationTests.cs
+++ src/tests/WooCommerce.NET.Tests/DeserializationTests.cs
@@ -20,26 +20,15 @@ namespace WooCommerce.NET.Tests
 			_v3RestApi = new RestAPI("wp-json/wc/v3", "", "");
 		}
 	
-		[TestCase(
-			@"ProductsJsonResponse_WhenDatesAreIncorrect.json",
-			"0001-01-01T00:00:00",
-			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreIncorrect")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenDatesAreCorrect.json",
-			"2023-11-29T19:03:58",
-			TestName = "DeserializeJSon_ProductsHaveCorrectDateTimeValues_WhenDatesAreCorrect")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenDatesAreNull.json",
-			"0001-01-01T00:00:00",
-			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreNull")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenDatesAreEmpty.json",
-			"0001-01-01T00:00:00",
-			TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreEmpty")]
+		[TestCase(@"ProductsJsonResponse_WhenDatesAreIncorrect.json", "0001-01-01T00:00:00", TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreIncorrect")]
+		[TestCase(@"ProductsJsonResponse_WhenDatesAreCorrect.json", "2023-11-29T19:03:58", TestName = "DeserializeJSon_ProductsHaveCorrectDateTimeValues_WhenDatesAreCorrect")]
+		[TestCase(@"ProductsJsonResponse_WhenDatesAreNull.json", "0001-01-01T00:00:00", TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreNull")]
+		[TestCase(@"ProductsJsonResponse_WhenDatesAreEmpty.json", "0001-01-01T00:00:00", TestName = "DeserializeJSon_ProductsHaveDateTimeMinValue_WhenDatesAreEmpty")]
 		public void DateTimeDeserializeJson(string fileName, string expectedDateValue)
 		{
 			// arrange
-			var jsonResponse = File.ReadAllText(_baseFilesPath + fileName);
+			var finalPath = Path.Combine(_baseFilesPath, fileName);
+			var jsonResponse = File.ReadAllText(finalPath);
 
 			// act
 			var products = _v3RestApi.DeserializeJSon<List<Product>>(jsonResponse);
@@ -61,26 +50,15 @@ namespace WooCommerce.NET.Tests
 			Assert.That(products[0].images[0].date_modified_gmt, Is.EqualTo(expectedDate));
 		}
 		
-		[TestCase(
-			@"ProductsJsonResponse_WhenManageStockValueIsParent.json",
-			false,
-			TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsParent")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenManageStockValueIsTrue.json",
-			true,
-			TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsTrue")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenManageStockValueIsNull.json",
-			false,
-			TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsNull")]
-		[TestCase(
-			@"ProductsJsonResponse_WhenManageStockValueIsFalse.json",
-			false,
-			TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsFalse")]
+		[TestCase(@"ProductsJsonResponse_WhenManageStockValueIsParent.json", false, TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsParent")]
+		[TestCase(@"ProductsJsonResponse_WhenManageStockValueIsTrue.json", true, TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsTrue")]
+		[TestCase(@"ProductsJsonResponse_WhenManageStockValueIsNull.json", false, TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsNull")]
+		[TestCase(@"ProductsJsonResponse_WhenManageStockValueIsFalse.json", false, TestName = "DeserializeJSon_ManageStockIsFalse_WhenManageStockValueIsFalse")]
 		public void ManageStockDeserializeJson(string fileName, bool expectedManageStock)
 		{
 			// arrange
-			var jsonResponse = File.ReadAllText(_baseFilesPath + fileName);
+			var finalPath = Path.Combine(_baseFilesPath, fileName);
+			var jsonResponse = File.ReadAllText(finalPath);
 
 			// act
 			var products = _v3RestApi.DeserializeJSon<List<Product>>(jsonResponse);
@@ -94,7 +72,8 @@ namespace WooCommerce.NET.Tests
 		public void DeserializeJSon_DoesNotThrowError_WhenReviewsAllowedIsIncorrect(string fileName)
 		{
 			// arrange
-			var jsonResponse = File.ReadAllText(_baseFilesPath + fileName);
+			var finalPath = Path.Combine(_baseFilesPath, fileName);
+			var jsonResponse = File.ReadAllText(finalPath);
 
 			// act / assert
 			Assert.DoesNotThrow(() => _v3RestApi.DeserializeJSon<List<Product>>(jsonResponse));
